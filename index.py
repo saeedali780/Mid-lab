26c7dbc3800f1eb83a2731871b0fc16f3ba9efe1
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-20 22:00:43 +0500

    final commit

--- index.py
+++ index.py
@@ -5,7 +5,7 @@
 # print("Hey" + youtube),
 # print("Hey {}".format(youtube) ),
 # print(f"Hey {youtube}" ),
-city = input("Enter city: "),
-papulation = input("Enter Papulation"),
-madlib = f"Pakistan's largest city is {city}! and it's Papulation is {papulation}",
-print(madlib),
+city = input("Enter city: ")
+papulation = input("Enter Papulation")
+madlib = f"Pakistan's largest city is {city}! and it's Papulation is {papulation}"
+print(madlib)
