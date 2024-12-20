2ed4e0799c14cf55b6074ad2c79ee75f384d555c
Saeed Ali <saeedaliofficial77@gmail.com>
2024-12-20 21:53:41 +0500

    madlib complete

---
+++ index.py
@@ -0,0 +1,11 @@
+# ====String concatination====
+
+# youtube = "Saeed"
+
+# print("Hey" + youtube),
+# print("Hey {}".format(youtube) ),
+# print(f"Hey {youtube}" ),
+city = input("Enter city: "),
+papulation = input("Enter Papulation"),
+madlib = f"Pakistan's largest city is {city}! and it's Papulation is {papulation}",
+print(madlib),
